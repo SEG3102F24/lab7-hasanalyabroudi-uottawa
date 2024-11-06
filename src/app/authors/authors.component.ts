4660eca60462e66dd80a862ef3a6c702ff5f8183
hasanalyabroudi-uottawa <halya054@uottawa.ca>
2024-11-06 10:50:40 -0500

    Completed Lab (Thought this code was already pushed)

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BooksService } from '../books/service/books.service';
+import { Author } from '../books/model/book';
 
 @Component({
   selector: 'app-authors',
-  standalone: true,
-  imports: [],
   templateUrl: './authors.component.html',
-  styleUrl: './authors.component.css'
+  styleUrls: ['./authors.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule]
 })
 export class AuthorsComponent {
+  authorId: string = '';
+  author: Author | null = null;
+  errorMessage: string = '';
 
+  constructor(private booksService: BooksService) {}
+
+  searchAuthor() {
+    if (this.authorId) {
+      this.booksService.getAuthorById(this.authorId).subscribe({
+        next: (data: Author) => {
+          this.author = data;
+          this.errorMessage = '';
+        },
+        error: (error) => {
+          console.error('Error fetching author:', error);
+          this.author = null;
+          this.errorMessage = 'Author not found';
+        }
+      });
+    }
+  }
 }
